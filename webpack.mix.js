072d381125e4558eaf1d6cc0fb4a4073855bea2b
unknown <brovkin.komar@yandex.ru>
2022-05-17 16:39:49 +0300

    В конфиге webpack.mix.js добавлен React

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,17 +1,11 @@
 const mix = require('laravel-mix');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
- | file for the application as well as bundling up all the JS files.
- |
- */
+mix
+  .js('resources/js/app.js', 'public/js')
+  .react()
+  .extract(['react'])
+  .browserSync('127.0.0.1:8000');
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+if (process.env.APP_ENV === 'production') {
+  mix.disableNotifications();
+}
