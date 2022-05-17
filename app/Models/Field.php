663a2e10818b2e5902965964b0dc97856ab3d05b
unknown <brovkin.komar@yandex.ru>
2022-05-17 11:31:38 +0300

    Добавлена модель поля

---
+++ app/Models/Field.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Field extends Model
+{
+    use HasFactory;
+    
+    public $timestamps = false;
+    protected $attributes = [
+        'name',
+        'description',
+        'form_uuid',
+        'type',
+    ];
+}
