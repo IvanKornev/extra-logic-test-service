bb7bec8c911d1710ad9f2a984885631a08874c76
unknown <brovkin.komar@yandex.ru>
2022-05-19 20:37:23 +0300

    Компонент ./components/field-editor/blocks/fields был отрефакторен

--- resources/js/src/components/field-editor/blocks/fields.jsx
+++ resources/js/src/components/field-editor/blocks/fields.jsx
@@ -6,20 +6,19 @@ import { fieldEditor, select } from '../../../domains';
 const Fields = ({ formikInstance }) => (
   <>
     { fieldEditor.fields.map((field) => {
-      const { name, label, component } = field;
+      const { name, label, component } = field;      
       const { isSelect, renderOptions } = select;
-
+      
+      const selectOptions = isSelect(component) && renderOptions(component);
       const CurrentComponent = component.name;
-        return(
-          <CurrentComponent
-            name={ name }
-            label={ label }
-            value={ formikInstance.values[name] }
-            variant="standard"
-            onChange={ formikInstance.handleChange }
-        >
-          { isSelect(component) && renderOptions(component) }
-        </CurrentComponent>
+      return(
+        <CurrentComponent
+          name={ name }
+          label={ label }
+          value={ formikInstance.values[name] }
+          variant="standard"
+          onChange={ formikInstance.handleChange }
+        >{ selectOptions }</CurrentComponent>
       );
     })}
   </>
