58b5b2129e255e884c3dbb54bf3b0a338eac7f2f
unknown <brovkin.komar@yandex.ru>
2022-05-19 15:31:12 +0300

    Добавлены базовые стили для редактора поля

--- resources/js/src/components/field-editor/field-editor.jsx
+++ resources/js/src/components/field-editor/field-editor.jsx
@@ -1,30 +1,64 @@
 import React from 'react';
-import { Formik, Form, Field } from 'formik';
-import { Button, Box } from '@mui/material';
+import { Formik, Form, useFormik } from 'formik';
+import { Button, Box, TextField, Select, MenuItem, Typography, Stack } from '@mui/material';
 
 import { newField } from '../../domains/new-field';
 import { styles } from './field-editor.styles';
 
 const FieldEditor = () => {
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      description: '',
+      type: 'text',
+    },
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+
   return(
     <Box component="section" sx={ styles.box }>
-      <Formik initialValues={ newField.initValues } onSubmit={ newField.create }>
+      <Typography variant="h5" component="h5">
+        Новое поле
+      </Typography>
+      <Formik onSubmit={ formik.handleSubmit }>
         <Form>
-          <Field name="name" placeholder="Название поля" />
-          <Field name="description" placeholder="Описание поля" />
-          <Field as="select" name="type">
-            <option value="text">Text</option>
-            <option value="textarea">TextArea</option>
-            <option value="select">Select</option>
-          </Field>
-          <div>
-            <Button variant="outlined" color="success" type="submit">
+          <Stack direction="row" spacing={ 2 }>
+            <TextField
+              name="name"
+              label="Название поля"
+              value={formik.values.name}
+              onChange={formik.handleChange}
+              variant="standard"
+              sx={ styles.field }
+            />
+            <TextField
+              name="description"
+              label="Описание"
+              value={formik.values.description}
+              variant="standard"
+              onChange={formik.handleChange}
+            />
+              <Select
+                value={ formik.values.type }
+                label="Тип поля"
+                variant="standard"
+                onChange={ formik.handleChange }
+              >
+                <MenuItem value={ "text" }>Текст (text)</MenuItem>
+                <MenuItem value={ "textarea" }>Текстовая зона (textarea)</MenuItem>
+                <MenuItem value={ "select" }>Селектор (select)</MenuItem>
+              </Select>
+          </Stack>
+          <Stack direction="row" spacing={2}>
+            <Button size="medium" variant="contained" color="success" type="submit">
               Сохранить поле
             </Button>
-            <Button variant="outlined" color="error">
+            <Button size="medium" variant="contained" color="error">
               Отмена
             </Button>
-          </div>
+          </Stack>
         </Form>
       </Formik>
     </Box>
--- resources/js/src/components/field-editor/field-editor.styles.js
+++ resources/js/src/components/field-editor/field-editor.styles.js
@@ -1,9 +1,19 @@
 const box = {
   p: 2,
-  border: '1px dashed grey',
+  background: 'rgba(0, 0, 0, 0.7)',
+  borderRadius: '3px',
   display: 'inline-flex',
+  flexDirection: 'column',
+  color: 'white',
+};
+
+const field = {
+  borderBottom: '1px solid white',
+  '& label, input': {
+    color: 'white',
+  },
 };
 
 export const styles = {
-  box,
+  box, field,
 };
