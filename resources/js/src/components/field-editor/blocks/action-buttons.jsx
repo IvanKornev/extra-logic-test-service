ca12a8b503efdcaf21fd4356419f4790f3469577
unknown <brovkin.komar@yandex.ru>
2022-05-19 21:11:43 +0300

    Добавлено закрытие окна редактирования поля

--- resources/js/src/components/field-editor/blocks/action-buttons.jsx
+++ resources/js/src/components/field-editor/blocks/action-buttons.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Button } from '@mui/material';
 
-const ActionButtons = () => (
+const ActionButtons = ({ abortCallback }) => (
   <>
-    <Button size="medium" variant="contained" color="success" type="submit">
-      Сохранить поле
-    </Button>
-    <Button size="medium" variant="contained" color="error">
-      Отмена
-    </Button> 
+    <Button
+      size="medium"
+      variant="contained"
+      color="success"
+      type="submit"
+    >Сохранить поле</Button>
+    <Button
+      size="medium"
+      variant="contained"
+      color="error"
+      onClick={ abortCallback }
+    >Отмена</Button> 
   </>
 );
+
+ActionButtons.propTypes = {
+  abortCallback: PropTypes.func.isRequired,
+};
 
 export { ActionButtons };
--- resources/js/src/components/field-editor/field-editor.jsx
+++ resources/js/src/components/field-editor/field-editor.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import { Formik, Form, useFormik } from 'formik';
 import { Box, Typography, Stack } from '@mui/material';
 
@@ -6,12 +8,12 @@ import { Fields, ActionButtons } from './blocks';
 import { newField } from '../../domains';
 import { styles } from './field-editor.styles';
 
-const FieldEditor = () => {
+const FieldEditor = props => {
   const formik = useFormik({
     initialValues: newField.initValues,
     onSubmit: newField.create,
   });
-
+  const { abortCallback } = props;
   return(
     <Box component="section" sx={ styles.box }>
       <Typography variant="h5" component="h5">
@@ -23,12 +25,16 @@ const FieldEditor = () => {
             <Fields formikInstance={ formik } />
           </Stack>
           <Stack direction="row" spacing={2}>
-            <ActionButtons />
+            <ActionButtons abortCallback={ abortCallback } />
           </Stack>
         </Form>
       </Formik>
     </Box>
   );
+};
+
+FieldEditor.propTypes = {
+  abortCallback: PropTypes.func.isRequired,
 };
 
 export { FieldEditor };
