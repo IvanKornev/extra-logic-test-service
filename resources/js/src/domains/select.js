29b12e9ff73ee00067be55fa248c3802b67056b3
unknown <brovkin.komar@yandex.ru>
2022-05-21 12:06:15 +0300

    Добавлены методы добавления, изменения и удаления опций в селекторе

--- resources/js/src/domains/select.js
+++ resources/js/src/domains/select.js
@@ -4,12 +4,41 @@ import generateId from 'uniqid';
 const renderOptions = component => (
   component.options.map((option) => (
     <MenuItem key={ generateId() } value={ option.value }>
-        { option.title }
+      { option.title }
     </MenuItem>
 )));
+
+const addOption = () => {
+  const title = prompt('Какое будет наименование у опции?', 'имя');
+  const value = prompt('А значение?', 'значение');
+  const id = generateId();
+  return { id, title, value };
+};
+
+const editOption = (id = 1, options) => {
+  const title = prompt('Какое будет наименование у опции?', 'имя');
+  const value = prompt('А значение?', 'значение');
+  return options.map((option) => {
+    if (option.id === id) {
+      option.title = title;
+      option.value = value;
+    }
+    return option;
+  });
+};
+
+const deleteOption = (id = 1, options) => (
+  options.filter((option) => option.id !== id)
+);
 
 const isSelect = component => (
   component.name === Select && true
 );
 
-export const select  = { renderOptions, isSelect };
+export const select  = {
+  renderOptions,
+  addOption,
+  editOption,
+  deleteOption,
+  isSelect,
+};
