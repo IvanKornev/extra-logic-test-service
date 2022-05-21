6e18b4ce0b0b2a3332b0522fe1fb1efa86ee7675
unknown <brovkin.komar@yandex.ru>
2022-05-21 17:04:53 +0300

    В папку ui-core добавлен компонент модального окна, который будет переиспользоваться во многих местах сервиса

---
+++ resources/js/src/ui-core/modal/modal.styles.js
@@ -0,0 +1,18 @@
+const modal = {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+};
+
+const box = {
+  p: 2,
+  background: 'rgba(255, 255, 255, 0.8)',
+  borderRadius: '6px',
+  display: 'inline-flex',
+  flexDirection: 'column',
+  color: 'black',
+};
+
+export const styles = {
+  modal, box,
+};
