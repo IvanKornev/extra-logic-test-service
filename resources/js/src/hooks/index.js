4e86ed4bcf77161209bc9710e8b0898345f627df
Ivan-KOrnev <brovkin.komar@yandex.ru>
2022-06-23 21:59:36 +0300

    Добавлен новый кастомный хук - useDrawer()

--- resources/js/src/hooks/index.js
+++ resources/js/src/hooks/index.js
@@ -2,3 +2,4 @@ export { useFieldsHandler } from './use-fields-handler';
 export { useSelectOptionsHandler } from './use-select-options-handler';
 export { useSnackbar } from './use-snackbar';
 export { useVisibilityManager } from './use-visibility-manager';
+export { useDrawer } from './use-drawer';
