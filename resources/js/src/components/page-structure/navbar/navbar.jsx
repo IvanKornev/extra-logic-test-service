15e560e73407cc0e4b2a80e62e0bc476e072f2e9
Ivan-KOrnev <brovkin.komar@yandex.ru>
2022-06-24 17:21:58 +0300

    Добавлена мобильная версия навигационной панели

--- resources/js/src/components/page-structure/navbar/navbar.jsx
+++ resources/js/src/components/page-structure/navbar/navbar.jsx
@@ -4,38 +4,48 @@ import { throttle } from 'lodash';
 
 import { form } from '@global-states';
 import { snackbarMessages, buttons } from '@constants';
-import { useSnackbar } from '@hooks';
+import { useSnackbar, useDrawer } from '@hooks';
 
 import { EventSnackbar } from '@components/reusable';
-import { Button, Typography, Tabs, Tab, Grid } from '@mui/material';
-import { UilFileAlt } from '@iconscout/react-unicons';
+import { Button, Typography, Tabs, Tab, Drawer } from '@mui/material';
+import { UilFileAlt, UilBars, UilPlusSquare } from '@iconscout/react-unicons';
 import styles from './navbar.module.scss';
 
-export const Navbar = observer(() => (
-  <nav className={styles['navbar']}>
-    <Grid columns={{ xs: 3 }} direction='row' alignItems='center' container>
-      <Grid gap={2} alignItems='center' direction='row' container item xs>
-        <UilFileAlt color='rgb(76, 43, 135)' size={52} />
-        <Typography variant='h6' component='h1'>
-          Новая форма
-        </Typography>
-      </Grid>
-      <Grid item container justifyContent='center' xs>
-        <Tabs
-          textColor='secondary'
-          indicatorColor='secondary'
-          value='create-form'>
-          <Tab value='create-form' label='Создать форму' />
-        </Tabs>
-      </Grid>
-      <Grid container item gap={2} justifyContent='flex-end' xs>
-        <NavbarButtons />
-      </Grid>
-    </Grid>
-  </nav>
-));
+export const Navbar = observer(() => {
+  const { isMobileDevice, drawerWasOpened, openDrawer } = useDrawer();
+  return (
+    <nav className={styles['navbar']}>
+      <div className={styles['navbar__wrapper']}>
+        <div className={styles['navbar__logo']}>
+          <UilFileAlt color='rgb(76, 43, 135)' size={52} />
+          <Typography variant='h6' component='h1'>
+            Новая форма
+          </Typography>
+        </div>
+        {!isMobileDevice && (
+          <NavbarInteractivePart />
+        )}
+        {isMobileDevice && (
+          <UilBars
+            color='rgb(76, 43, 135)'
+            onClick={() => openDrawer(true)}
+            size={36}
+          />
+        )}
+      </div>
+      <Drawer
+        anchor='left'
+        onClose={() => openDrawer(false)}
+        open={drawerWasOpened}>
+        <div className={styles['drawer__wrapper']}>
+          <NavbarInteractivePart isMobileDevice={isMobileDevice} />
+        </div>
+      </Drawer>
+    </nav>
+  );
+});
 
-const NavbarButtons = observer(() => {
+const NavbarInteractivePart = observer(({ isMobileDevice }) => {
   const [wasThrottled, setThrottlingStatus] = useState(false);
   const { message, showSnackbar, snackbarRef } = useSnackbar();
   const handleButton = (actionName) => () => {
@@ -46,23 +56,43 @@ const NavbarButtons = observer(() => {
   };
   return (
     <>
-      {buttons.navbar.map((button) => {
-        const isDisable = wasThrottled || !form.fieldsCounter;
-        const clickHandler = handleButton(button.action);
-        return (
-          <Button
-            key={useId()}
-            id={`navbar__button_${button.action}`}
-            color={button.color}
-            variant='outlined'
-            type='button'
-            disabled={isDisable}
-            onClick={throttle(clickHandler, 1500)}>
-            {button.text}
-          </Button>
-        );
-      })}
-      <EventSnackbar message={message} ref={snackbarRef} />
+      <div className={styles['navbar__items']}>
+        {isMobileDevice && (
+          <div className={styles['navbar__item']}>
+            <UilPlusSquare color='rgb(76, 43, 135)' size={30} />
+            <Typography variant='h6' component='h2'>
+              Создать форму
+            </Typography>
+          </div>  
+        )}
+        {!isMobileDevice && (
+          <Tabs
+            textColor='secondary'
+            indicatorColor='secondary'
+            value='to-create-form'>
+            <Tab value='to-create-form' label='Создать форму' />
+          </Tabs>
+        )}
+      </div>
+      <div className={styles['navbar__buttons']}>
+        {buttons.navbar.map((button) => {
+          const isDisable = wasThrottled || !form.fieldsCounter;
+          const clickHandler = handleButton(button.action);
+          return (
+            <Button
+              key={useId()}
+              id={`navbar__button_${button.action}`}
+              color={button.color}
+              variant={isMobileDevice ? 'contained' : 'outlined'}
+              disabled={isDisable}
+              type='button'
+              onClick={throttle(clickHandler, 1500)}>
+              {button.text}
+            </Button>
+          );
+        })}
+        <EventSnackbar message={message} ref={snackbarRef} />
+      </div>
     </>
   );
 });
--- resources/js/src/hooks/use-drawer.js
+++ resources/js/src/hooks/use-drawer.js
@@ -4,7 +4,7 @@ import useMediaQuery from '@mui/material/useMediaQuery';
 export const useDrawer = (initialState = false) => {
   const isMobileDevice = useMediaQuery('(max-width:768px)');
   const [drawerWasOpened, openDrawer] = useState(initialState);
-  
+
   useEffect(() => {
     if (!isMobileDevice) {
       openDrawer(false);
