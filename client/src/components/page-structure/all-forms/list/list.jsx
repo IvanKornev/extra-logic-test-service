93b5e08a356c59b6857db608544f267697809146
Ivan-KOrnev <brovkin.komar@yandex.ru>
2022-07-18 21:28:34 +0300

    Теперь, если пользователь не авторизован, вместо сохраненных форм он видит баннер с уведомлением

--- client/src/components/page-structure/all-forms/list/list.jsx
+++ client/src/components/page-structure/all-forms/list/list.jsx
@@ -1,22 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
 import { useNavigate } from 'react-router-dom';
-import { usePageNavigator } from '@hooks';
+
+import { observer } from 'mobx-react-lite';
+import { userState } from '@global-states';
 
 import { getAllForms } from '@api';
-import { useQuery } from '@hooks';
+import { useQuery, usePageNavigator } from '@hooks';
 
 import { FormCard } from '@components/reusable';
 import { Typography, Skeleton } from '@mui/material';
+import { UilLockAlt } from '@iconscout/react-unicons';
 import styles from './list.module.scss';
 
-export const AllFormsList = ({ userId }) => {
-  const queryResults = useQuery(getAllForms(userId), [userId]);
-  const navigate = useNavigate();
-
-  const { formsList } = queryResults;
-  const skeletonCardsId = Array.from(Array(10).keys());
+export const AllFormsList = observer(({ userId }) => {
   return (
     <div className={styles['all-forms__list']}>
       <div className={styles['list__wrapper']}>
@@ -25,35 +22,63 @@ export const AllFormsList = ({ userId }) => {
             Формы
           </Typography>
         </div>
-        <div className={styles['list__cards']}>
-          {!formsList.length &&
-            skeletonCardsId.map((id) => (
-              <Skeleton
-                height={150}
-                width={150}
-                key={id}
-                variant='rectangular'
-                animation='wave'
-              />
-            ))}
-          {formsList.map((form) => {
-            const clickHandler = () => {
-              const { id } = form;
-              usePageNavigator(navigate, 'saving-form')(id);
-            };
-            return (
-              <FormCard
-                onClick={clickHandler}
-                key={form.id}
-                form={form}
-              />
-            );
-          })}
-        </div>
+        {!userState.isAuthorized && (
+          <UnauthorizedUserBanner />
+        )}
+        {userState.isAuthorized && (
+          <AuthorizedUserList userId={userId} />
+        )}
       </div>
     </div>
   );
-};
+});
+
+const UnauthorizedUserBanner = () => (
+  <div className={styles['banner']}>
+    <div className={styles['banner__wrapper']}>
+      <UilLockAlt size={50} />
+      <Typography>
+        Просмотр сохраненных форм доступен лишь
+        авторизованным пользователям
+      </Typography>
+    </div>
+  </div>
+);
+
+const AuthorizedUserList = ({ userId }) => {
+  const queryResults = useQuery(getAllForms(userId), [userId]);
+  const navigate = useNavigate();
+
+  const { formsList } = queryResults;
+  const skeletonCardsId = Array.from(Array(10).keys());
+  return (
+    <div className={styles['list__cards']}>
+      {!formsList.length &&
+        skeletonCardsId.map((id) => (
+          <Skeleton
+            height={150}
+            width={150}
+            key={id}
+            variant='rectangular'
+            animation='wave'
+          />
+        ))}
+      {formsList.map((form) => {
+        const clickHandler = () => {
+          const { id } = form;
+          usePageNavigator(navigate, 'saving-form')(id);
+        };
+        return (
+          <FormCard
+            onClick={clickHandler}
+            key={form.id}
+            form={form}
+          />
+        );
+      })}
+    </div>
+  );
+}
 
 AllFormsList.propTypes = {
   userId: PropTypes.number,
